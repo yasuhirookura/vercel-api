8a983cf437078daa91984a22ffb7788cb026c6c7
github yasuhirookura <baseball.yasu.ohkura@gmail.com>
2026-03-20 15:23:28 +0900

    rename api file to generate

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,16 @@
 export default async function handler(req, res) {
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+return res.status(200).end();
+}
+
+if (req.method !== "POST") {
+return res.status(405).json({ error: "Method not allowed" });
+}
+
 try {
 const { input } = req.body;
 
@@ -24,8 +36,8 @@ ${input}`
 });
 
 const data = await response.json();
-res.status(200).json(data);
+return res.status(200).json(data);
 } catch (error) {
-res.status(500).json({ error: error.message });
+return res.status(500).json({ error: error.message });
 }
 }
